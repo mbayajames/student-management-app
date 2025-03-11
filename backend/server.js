acf955dfedbc501b140d6b519006cf59d437fcc5
mbayajames <jmuthiora244@gmail.com>
2025-03-11 02:46:22 -0700

    Updated backend server configuration in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
 const dotenv = require('dotenv');
 
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT  || 5000;
+const cors = require('cors');
+const PORT = process.env.PORT  || 4000;
+
+
 
 //Middleware
 app.use(cors());
 app.use(express.json());
 
 //MongoDB connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
- .then(() => console.log('MongoDB connected'))
- .catch(err => console.log(error));
+mongoose.connect('mongodb://localhost:27017/yourDBName', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 
  //Routes
