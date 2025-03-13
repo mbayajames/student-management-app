768955488138da72f7faf7a57ffc83da4aa85992
mbayajames <jmuthiora244@gmail.com>
2025-03-13 04:37:36 -0700

    Updated StudentModels.js in models/ to improve schema structure and functionality

--- backend/models/StudentModels.js
+++ backend/models/StudentModels.js
@@ -1,10 +1,20 @@
-const mongoose = require('mongoose');
+const Student_controller = req('../Controlers/studentController.js');
+const express = req('express');
 
-const StudentSchema = new mongoose.Schema ({
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    gender: { type: String, required: true, enum: ['Male', 'Female', 'Other']
-},
-});
+const Student_Route = express.Router();
 
-module.exports = mongoose.model('Student', StudentSchema);
+Student_Route.get('/getAllStudents', Student_controller.getAllStudents);
+
+
+Student_Route.post('/addStudents', Student_controller.addStudent);
+
+
+Student_Route.get('/getStudentById/id:', Student_controller.getStudentById);
+
+
+Student_Route.patch('/updateStudent/id:', Student_controller.updateStudent);
+
+
+Student_Route.delete('/deleteStudent/id:', Student_controller.deleteStudent);
+
+module.exports = Student_Route;
