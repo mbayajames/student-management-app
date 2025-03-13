a3d3352a01adec93df8b56ed7fc30b0fa366c874
mbayajames <jmuthiora244@gmail.com>
2025-03-13 05:41:20 -0700

    Modified server.js to improve functionality and optimize performance

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,46 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const express = require("express"); //! to import express
+const rateLimiter = require("express-rate-limit") //! to import rate limiter
+require("dotenv").config(); //! to import dotenv
+require("helmet");
 
+require("./Helpers/init_mongodb"); //! to import init_mongodb from Helpers folder
+const app = express(); //! to use express
 
-dotenv.config();
+const limiter = rateLimiter({
+    max: 3, //! max number of requests
+    windowMs: 60 * 60 * 1000, //! time window in milliseconds
+    message: "Too many requests from this IP, please try again later"
+})
 
-const app = express();
-const cors = require('cors');
-const PORT = process.env.PORT  || 4000;
+app.use("/api", limiter); //! to use rate limiter on /api route
 
+//! Importing Routes:
+const Student_Routes = require("./routes/StudentsRoutes");
+const Student_Auth_Routes = require("./Routes/studentAuthRoutes");
 
-
-//Middleware
-app.use(cors());
+//! to use body-parser
 app.use(express.json());
 
-//MongoDB connection
-mongoose.connect('mongodb://localhost:27017/yourDBName', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+
+app.use("/api/students", Student_Routes);
+app.use("/api/studentAuth", Student_Auth_Routes);
+
+app.use(Student_Routes); //! Using the Imported Routes:
+app.use(Lecturer_routes);
+app.use(Student_Auth_Routes);
+
+// app.use('students')
+
+//! handling 404 errors
+app.use(async (request, response, next) => {
+    next(createError.NotFound())
 })
-.then(() => console.log('MongoDB Connected'))
-.catch(err => console.error('MongoDB connection error:', err));
 
+app.use((error, request, response, next) => {
+    response.status
+})
 
- //Routes
-//  app.use('/api/auth', require('./routes/auth'));
- app.use('/api/students', require('routes/students'));
-
- app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+//! Starting the Server:
+app.listen(process.env.port || 4000, function () {
+    console.log("Now listening for requests on: http://localhost:4000"); //! to print the server is running on port 4000
+});
