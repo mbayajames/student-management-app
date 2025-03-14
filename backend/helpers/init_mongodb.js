a7e7e55d0cbbab8abe40547106ca1fb551d9e679
mbayajames <jmuthiora244@gmail.com>
2025-03-14 05:31:51 -0700

    Update init_mongodb helper: Improved database initialization logic

---
+++ backend/helpers/init_mongodb.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URI, {dbName: process.env.DB_NAME})
+.then(()=>{
+    console.log('mongodb connected')
+})
+
+.catch((err)=>console.log(err.message));
