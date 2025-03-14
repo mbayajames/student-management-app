b2f0b6d7a73072701ac36df93dec0354a9692b4b
mbayajames <jmuthiora244@gmail.com>
2025-03-14 05:35:51 -0700

    Update students model: Improved schema structure and validation

---
+++ backend/models/studentsModels.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const studentSchema = new Schema({
+    firstname:{
+        type:String,
+        require:[true, 'Firstname is required']
+    },
+    lastname:{
+        type:String,
+        require:[true, 'Lastname is required']
+    },
+    gender:{
+        type: String
+    },
+    // role:{
+    //     type: DataTypes.ENUM('admin', 'nan1', 'student'),
+    //     defaultValue: 'student'
+    // },
+});
+
+const Student = mongoose.module('student', studentSchema);
+module.exports = Student;
